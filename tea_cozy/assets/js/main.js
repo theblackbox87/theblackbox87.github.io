ed8530a1772e0000c29e0c99af98234af28599c9
theblackbox87 <francesco@fsgraphics.it>
2020-04-26 13:18:34 +0200

    update 26-04-2020

---
+++ tea_cozy/assets/js/main.js
@@ -0,0 +1,35 @@
+function smoothScroll(target,duration){
+  var target = document.querySelector(target);
+  var targetPosition = target.getBoundingClientRect().top;
+  var startPosition = window.pageYOffset;
+  var distance = targetPosition - startPosition;
+  var startTime = null;
+  function animation(currentTime){
+    if(startTime===null) startTime = currentTime;
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed,startPosition,distance,duration);
+    window.scrollTo(0,run);
+    if(timeElapsed < duration) requestAnimationFrame(animation);    
+  }
+
+function ease (t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t + b;
+	t--;
+	return -c/2 * (t*(t-2) - 1) + b;
+}
+
+  requestAnimationFrame(animation);
+}
+
+
+
+var scroll_btn = document.querySelector('#featured-btn');
+scroll_btn.addEventListener('click',function(){
+  smoothScroll('.featured-tea',1000);
+});
+
+var scroll_btn = document.querySelector('#locations-btn');
+scroll_btn.addEventListener('click',function(){
+  smoothScroll('.locations',1000);
+});
